52426cda464f1541df73f0b955c1113b12fb7944
Jayachandran <79501478+JCjaiRN@users.noreply.github.com>
2022-03-08 16:49:20 +0530

    Add files via upload

---
+++ writes data to the SD Card/MainActivity.java
@@ -0,0 +1,76 @@
+packagecom.example.exno8;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity; 
+import android.view.View;
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.Toast;
+import java.io.BufferedReader; 
+import java.io.File;
+import java.io.FileInputStream; 
+import java.io.FileOutputStream; 
+import java.io.InputStreamReader;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity
+{ EditText E1;
+Button write,read,clear; 
+@Override
+protected void onCreate(Bundle savedInstanceState)
+{
+super.onCreate(savedInstanceState); 
+setContentView(R.layout.activity_main);
+E1= (EditText) findViewById(R.id.editText); 
+write= (Button) findViewById(R.id.button); 
+read= (Button) findViewById(R.id.button2); 
+clear= (Button) findViewById(R.id.button3);
+write.setOnClickListener(new View.OnClickListener()
+{
+@Override
+public void onClick(View v)
+{ String message=E1.getText().toString(); 
+try {
+File f=new File("/sdcard/myfile.txt"); 
+f.createNewFile();
+FileOutputStream fout=new FileOutputStream(f); 
+fout.write(message.getBytes());
+fout.close();
+Toast.makeText(getBaseContext(),"Data Written in SDCARD",Toast.LENGTH_LONG).show();
+}
+catch (Exception e)
+{ }
+});
+Toast.makeText(getBaseContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+}
+read.setOnClickListener(new View.OnClickListener()
+{
+@Override
+public void onClick(View v)
+{
+String message; 
+String buf = ""; 
+try
+{
+File f = new File("/sdcard/myfile.txt"); 
+FileInputStream fin = new FileInputStream(f);
+BufferedReader br = new BufferedReader(new InputStreamReader(fin));
+while ((message = br.readLine()) != null)
+{
+buf += message;
+}E1.setText(buf); 
+br.close();
+fin.close();
+Toast.makeText(getBaseContext(),"Data Recived from SDCARD",Toast.LENGTH_LONG).show();
+}
+catch (Exception e)
+{ }
+});
+Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+}
+clear.setOnClickListener(new View.OnClickListener()
+{
+@Override
+public void onClick(View v)
+{ E1.setText("");
+}
+});
+} }
